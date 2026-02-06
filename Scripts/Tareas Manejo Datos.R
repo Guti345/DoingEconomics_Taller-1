384ba8a4782b0a4f39e634d6fca69f622b77ec47
Antonio Gutierrez <antonio.gutierrez@urosario.edu.co>
2026-02-06 00:15:32 -0500

    Update Tareas Manejo Datos.R

--- Scripts/Tareas Manejo Datos.R
+++ Scripts/Tareas Manejo Datos.R
@@ -1,14 +1,43 @@
 setwd("GitHub/DoingEconomics_Taller-1")
 
 library(haven)
+library(dplyr)
+library(tidyr)
 TenderosFU03_Publica <- read_dta("Data/Raw/TenderosFU03_Publica.dta")
 View(TenderosFU03_Publica)
 
-internet <- TenderosFU03_Publica %>%
+# Tarea 1
+
+internet_muni <- TenderosFU03_Publica %>%
   select(Munic_Dept, uso_internet, Municipio) %>%
   group_by(Munic_Dept, Municipio) %>%
   summarise(p_int = sum(uso_internet)/n() * 100)
 
-View(internet)
+View(internet_muni)
 
+# Tarea 2
 
+lista_sec <- c(
+  "actG1"  = "Tienda",
+  "actG2"  = "Comida preparada",
+  "actG3"  = "Peluquería y belleza",
+  "actG4"  = "Ropa",
+  "actG5"  = "Otras variedades",
+  "actG6"  = "Papelería y comunicaciones",
+  "actG7"  = "Vida nocturna",
+  "actG8"  = "Productos bajo inventario",
+  "actG9"  = "Salud",
+  "actG10" = "Servicios",
+  "actG11" = "Ferretería y afines"
+)
+
+internet_sectores <- TenderosFU03_Publica %>%
+  select(uso_internet, actG1:actG11) %>%
+  pivot_longer(cols = actG1:actG11, names_to = "sectores", values_to = "valor") %>%
+  filter(valor == 1) %>%
+  select(-valor) %>%
+  mutate(sectores = recode(sectores, !!!lista_sec)) %>%
+  group_by(sectores) %>%
+  summarise(p_int = sum(uso_internet)/n() * 100) %>%
+  arrange(desc(p_int))
+  
